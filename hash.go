0aabb68816ddef2d721a60bc877726439130183b
Umer30962 <umer30962@gmail.com>
2026-04-07 23:29:48 +0500

    Refactor main.go to enhance error handling and improve logging for better debugging and maintenance.

---
+++ hash.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+    password := "admin123456" // new super admin password
+    hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    fmt.Println(string(hash))
+}
